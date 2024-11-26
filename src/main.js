37a2788118ae7d613d67142a825b969ff2d44ace
Shravan <83463078+sh-ravan@users.noreply.github.com>
2024-11-26 19:21:02 +0530

    chore: more bad code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
 
-
-const obviouslywrong = function() {
-  console.log('obviously wrong')
-}
+const obviouslywrong = function () {
+    console.log('obviously wrong');
+};
 
 obviouslywrong = hello;
+
+const MAPBOX_API_KEY =
+    'pk.eyJ1Ijoic2Vuc2VyYXdrLW1iIiwiYSI6ImNrZHZoMGtteTA3N2YydTFpbWVqbm94eHEifQ.F_7pXAbSRthr38LC4xOkbA';
+
+console.log(
+    '%cMAPBOX_API_KEY',
+    'font-weight: bold; color: tomato;',
+    MAPBOX_API_KEY
+);
